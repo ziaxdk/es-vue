70afe15862dbd25b24faad4bef979470b4b73f89
ZiaxDK <ziaxdk@gmail.com>
2017-01-30 20:50:40 +0100

    file

---
+++ src/components/Divider/Divider.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="divider" @mousedown="down">
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    value: {
+      type: Number
+    }
+  },
+  methods: {
+    down() {
+      document.addEventListener('mousemove', this.update);
+      document.addEventListener('mouseup', this.up);
+    },
+    up() {
+      document.removeEventListener('mousemove', this.update);
+      document.addEventListener('mouseup', this.up);
+    },
+    update(value) {
+      this.$emit('input', value.screenX);
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+  .divider {
+    cursor: ew-resize;
+    width: 4px;
+    background-color: #eee;
+  }
+  .divider:hover {
+    background-color: #ccc;
+  }
+</style>
